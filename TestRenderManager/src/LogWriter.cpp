4657a2114dfc106ec032abda573f3b10ba0ca9fe
Xavier Corbillon <xavier.corbillon@telecom-bretagne.eu>
2017-01-17 18:55:18 +0100

    osvr_client: multithread implementation of the LogWriter

--- TestRenderManager/src/LogWriter.cpp
+++ TestRenderManager/src/LogWriter.cpp
@@ -9,6 +9,11 @@
 
 using namespace IMT;
 
+using LockGuard = std::lock_guard<std::mutex>;
+using UniqueLock = std::unique_lock<std::mutex>;
+
+#define SWAP_LIMIT 10
+
 void LogWriter::AddLog(const Log& log)
 {
   if (m_isRunning)
@@ -20,7 +25,11 @@ void LogWriter::AddLog(const Log& log)
     }
     if (m_lastTimestamp < log.GetTimestamp())
     {
-      *m_output << log-m_startTimestamp << "\n";
+      m_logQueue.push(log-m_startTimestamp);
+      if (m_logQueue.size() > SWAP_LIMIT)
+      {
+        SwapBuffer();
+      }
       m_lastTimestamp = log.GetTimestamp();
     }
   }
@@ -33,6 +42,7 @@ void LogWriter::Start(void)
     m_isRunning = true;
     m_firstTimestamp = true;
     m_output = std::make_shared<std::ofstream>(m_storageFolder + "/" + m_logId + "_" + std::to_string(m_testId)+".txt");
+    m_writingThread = std::thread(&LogWriter::Writer, this);
   }
 }
 
@@ -40,10 +50,51 @@ void LogWriter::Stop(void)
 {
   if (m_isRunning)
   {
-    std::cout << "Stop" << std::endl;
-    m_isRunning = false;
-    *m_output << std::flush;
-    m_output = nullptr;
+    {
+      UniqueLock locker(m_mutex);
+      std::cout << "Stop" << std::endl;
+      m_isRunning = false;
+      SwapBuffer(false);
+      m_cv.wait(locker, [this](){return m_output == nullptr;});
+    }
     ++m_testId;
+    m_writingThread.join();
+  }
+}
+
+void LogWriter::SwapBuffer(bool withLock)
+{
+  if (withLock)
+  {
+    UniqueLock locker(m_mutex);
+    std::swap(m_logQueue, m_writerLogQueue);
+    m_cv.notify_one();
+  }
+  else
+  {
+    std::swap(m_logQueue, m_writerLogQueue);
+    m_cv.notify_one();
+  }
+}
+
+
+void LogWriter::Writer(void)
+{
+  while(true)
+  {
+    UniqueLock locker(m_mutex);
+    m_cv.wait(locker, [this](){return !m_writerLogQueue.empty() > 0 || !m_isRunning;});
+    while(!m_writerLogQueue.empty())
+    {
+      *m_output << m_writerLogQueue.front() << "\n";
+      m_writerLogQueue.pop();
+    }
+    if (!m_isRunning)
+    { // if the loger is not running anymore we join the thread;
+      *m_output << std::flush;
+      m_output = nullptr;
+      m_cv.notify_one();
+      return;
+    }
   }
 }
